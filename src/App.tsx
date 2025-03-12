5d101f1a0c2e8d4d35847899e6b15e03b6b44b1d
gyuchanlee <hoo788@gmail.com>
2025-03-12 12:02:18 +0900

    2025-03-10 개인 프로젝트 - ㅇㅇ

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,12 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useAuthStore } from './stores/authStore';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import RoomList from './pages/ChatRoomList';
-import ChatRoom from './pages/ChatRoom';
-import ProtectedRoute from './components/common/ProtectedRouter';
+import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-    const { checkAuthStatus } = useAuthStore();
-
-    // 앱 초기화 시 인증 상태 확인
-    useEffect(() => {
-        const initAuth = async () => {
-            await checkAuthStatus();
-        };
-
-        initAuth();
-    }, [checkAuthStatus]);
 
     return (
-        <Router>
-            <div className="app">
-                <Routes>
-                    {/* 인증 라우트 */}
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-
-                    {/* 보호된 라우트 */}
-                    <Route
-                        path="/rooms"
-                        element={
-                            <ProtectedRoute>
-                                <RoomList />
-                            </ProtectedRoute>
-                        }
-                    />
-
-                    <Route
-                        path="/rooms/:roomId"
-                        element={
-                            <ProtectedRoute>
-                                <ChatRoom />
-                            </ProtectedRoute>
-                        }
-                    />
-
-                    {/* 기본 리다이렉트 */}
-                    <Route path="/" element={<Navigate to="/rooms" replace />} />
-                    <Route path="*" element={<Navigate to="/rooms" replace />} />
-                </Routes>
-            </div>
-        </Router>
+        <>
+            <div>ㅎㅇ</div>
+        </>
     );
 };
 
